6edfaefc185597d8d5ac776a6efb30c76640ed75
Tronmatrix 81 <arrobajustie@gmail.com>
2021-03-04 20:53:50 -0700

    Optimization and Data Base basement

--- Cerrado automático/Form1.vb
+++ Cerrado automático/Form1.vb
@@ -1,16 +1,16 @@
 ﻿Public Class Formulario
+    Private Const M1 As String = "-----.-..... -.-.----..."
+    Private Const entrar As String = "@gmail.comhello"
+    Private Const existente As String = "@gmail.com"
 
-    Private Sub BINGRESAR_Click(sender As System.Object, e As System.EventArgs) Handles BINGRESAR.Click
-        Dim entrar As String
-        Dim morse As String
-        entrar = "arrobajustie@gmail.comhello"
-        morse = "-----.-.....-.-.----..."
+    Private Sub BINGRESAR_Click(sender As Object, e As EventArgs) Handles BINGRESAR.Click
+
         If TBCORREO.Text & TBCONTRASENA.Text = entrar Then
             Timer1.Stop()
-            MsgBox(("Bienvenido"(TBCORREO.Text)), vbInformation, "PSN")
+            MsgBox("Bienvenido", vbInformation, "PSN")
             Close()
         End If
-        If TBCORREO.Text = morse Then
+        If TBCORREO.Text = M1 Then
             Timer1.Stop()
             MsgBox("Welcome", vbInformation, "PSN")
             Close()
@@ -22,7 +22,7 @@
         End If
     End Sub
 
-    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         LMS.Text -= 1
         If LMS.Text <= 0 Then
             LMS.Text = 9
@@ -42,21 +42,26 @@
         End If
     End Sub
 
-    Private Sub Formulario_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    Private Sub Formulario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
         LMS.Text = 9
         LS.Text = 59
         LM.Text = 1
     End Sub
 
-    Private Sub BCREAR_Click(sender As System.Object, e As System.EventArgs) Handles BCREAR.Click
+    Private Sub BCREAR_Click(sender As Object, e As EventArgs) Handles BCREAR.Click
+        'If TBCORREO.Text = existente Then
+        '    MsgBox("Correo ya existente", vbCritical, "Log In")
+        'Else
         If TBCORREO.Text & TBCONTRASENA.Text = "" Then
             Timer1.Stop()
             MsgBox("Debes completar los datos requeridos primero", vbCritical, "Registrarse")
             Timer1.Start()
         End If
-        If TBCORREO.Text <> "" Then
-            If TBCONTRASENA.Text <> "" Then
+        'End If
+        If TBCORREO.Text IsNot "" Then
+            If TBCONTRASENA.Text IsNot "" Then
+                'If TBCORREO.Text IsNot existente Then
                 Timer1.Stop()
                 MsgBox("Bienvenido", vbInformation, "Registrado")
             Else
@@ -65,8 +70,10 @@
                 Timer1.Start()
             End If
         End If
-        If TBCONTRASENA.Text <> "" Then
-            If TBCORREO.Text <> "" Then
+        'End If
+        'If TBCORREO.Text IsNot existente Then
+        If TBCONTRASENA.Text IsNot "" Then
+            If TBCORREO.Text IsNot "" Then
                 Close()
             Else
                 Timer1.Stop()
@@ -74,5 +81,6 @@
                 Timer1.Start()
             End If
         End If
+        'End If
     End Sub
 End Class

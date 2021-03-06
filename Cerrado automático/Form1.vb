41769664658dd9dd330a3769c995499101bbe0d2
Tronmatrix 81 <arrobajustie@gmail.com>
2021-03-06 13:35:24 -0700

    ¡DataBase bug Fixed!; everything is running fine.

--- Cerrado automático/Form1.vb
+++ Cerrado automático/Form1.vb
@@ -1,6 +1,5 @@
 ﻿Imports System.Data.OleDb
 Imports System.Data
-
 Public Class Formulario
     Dim connection As New OleDbConnection(My.Settings.DataBase2ConnectionString)
     Private Sub BINGRESAR_Click(sender As Object, e As EventArgs) Handles BINGRESAR.Click
@@ -13,21 +12,20 @@ Public Class Formulario
             If connection.State = ConnectionState.Closed Then
                 connection.Open()
             End If
-            Dim cmd As New OleDbCommand("select count (*) from tabla1 where Correo=? and CONTRASENA=?", connection)
-            cmd.Parameters.AddWithValue("@1", OleDbType.VarChar).Value = TBCORREO.Text
-            cmd.Parameters.AddWithValue("@2", OleDbType.VarChar).Value = TBCONTRASENA.Text
-            Dim count = Convert.ToInt32(cmd.ExecuteScalar())
-
-            If (count > 0) Then
+            Dim cmd As New OleDbCommand("SELECT COUNT (*) from tabla1 where Correo=? and Contrasena=?", connection)
+            cmd.Parameters.AddWithValue("Correo", TBCORREO.Text)
+            cmd.Parameters.AddWithValue("Contrasena", TBCONTRASENA.Text)
+            If Convert.ToInt32(cmd.ExecuteScalar()) > 0 Then
                 Timer1.Stop()
                 MsgBox("Bienvenid@ " & TBCORREO.Text, vbInformation, "PSN")
+                connection.Close()
                 Close()
             Else
                 Timer1.Stop()
-                MsgBox("Contraseña y/o correo incorrectos", vbExclamation, "Error")
-                TBCONTRASENA.Text = ""
-                Timer1.Start()
-            End If
+                    MsgBox("Contraseña y/o correo incorrectos", vbExclamation, "Error")
+                    TBCONTRASENA.Text = ""
+                    Timer1.Start()
+                End If
         End If
     End Sub
 
@@ -63,5 +61,25 @@ Public Class Formulario
         TBCORREO.Text = ""
         TBCONTRASENA.Text = ""
         Registro.ShowDialog()
+    End Sub
+
+    Private Sub TBCORREO_TextChanged(sender As Object, e As EventArgs) Handles TBCORREO.TextChanged
+        If TBCORREO.Text <> Nothing Then
+            LCORREO.Text = ""
+        Else
+            LCORREO.Text = "Correo electrónico"
+        End If
+    End Sub
+
+    Private Sub TBCONTRASENA_TextChanged(sender As Object, e As EventArgs) Handles TBCONTRASENA.TextChanged
+        If TBCONTRASENA.Text <> Nothing Then
+            LCONTRASENA.Text = ""
+        Else
+            LCONTRASENA.Text = "Contraseña"
+        End If
+    End Sub
+
+    Private Sub Formulario_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        connection.Close()
     End Sub
 End Class
--- Cerrado automático/Form2.Designer.vb
+++ Cerrado automático/Form2.Designer.vb
@@ -30,12 +30,12 @@ Partial Class Registro
         Me.TBCORREO = New System.Windows.Forms.TextBox()
         Me.TBCONTRASENA = New System.Windows.Forms.TextBox()
         Me.TB1 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LUSUARIO = New System.Windows.Forms.Label()
+        Me.LCONTRASENA = New System.Windows.Forms.Label()
+        Me.L1 = New System.Windows.Forms.Label()
         Me.BCREAR = New System.Windows.Forms.Button()
         Me.BCANCELAR = New System.Windows.Forms.Button()
+        Me.LCORREO = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -53,11 +53,11 @@ Partial Class Registro
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(227, 52)
+        Me.Label6.Location = New System.Drawing.Point(227, 49)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(119, 20)
+        Me.Label6.Size = New System.Drawing.Size(71, 20)
         Me.Label6.TabIndex = 28
-        Me.Label6.Text = "Es fácil y rápido"
+        Me.Label6.Text = "Es gratis"
         '
         'Label5
         '
@@ -73,87 +73,82 @@ Partial Class Registro
         '
         Me.TBUSUARIO.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TBUSUARIO.ForeColor = System.Drawing.Color.Gray
-        Me.TBUSUARIO.Location = New System.Drawing.Point(231, 84)
-        Me.TBUSUARIO.Multiline = True
+        Me.TBUSUARIO.Location = New System.Drawing.Point(231, 100)
         Me.TBUSUARIO.Name = "TBUSUARIO"
-        Me.TBUSUARIO.Size = New System.Drawing.Size(135, 27)
+        Me.TBUSUARIO.Size = New System.Drawing.Size(135, 22)
         Me.TBUSUARIO.TabIndex = 35
         '
         'TBCORREO
         '
         Me.TBCORREO.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TBCORREO.ForeColor = System.Drawing.Color.Gray
-        Me.TBCORREO.Location = New System.Drawing.Point(231, 133)
-        Me.TBCORREO.Multiline = True
+        Me.TBCORREO.Location = New System.Drawing.Point(231, 140)
         Me.TBCORREO.Name = "TBCORREO"
-        Me.TBCORREO.Size = New System.Drawing.Size(276, 27)
+        Me.TBCORREO.Size = New System.Drawing.Size(276, 22)
         Me.TBCORREO.TabIndex = 36
         '
         'TBCONTRASENA
         '
         Me.TBCONTRASENA.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TBCONTRASENA.ForeColor = System.Drawing.Color.Gray
-        Me.TBCONTRASENA.Location = New System.Drawing.Point(231, 186)
-        Me.TBCONTRASENA.Multiline = True
+        Me.TBCONTRASENA.Location = New System.Drawing.Point(231, 184)
         Me.TBCONTRASENA.Name = "TBCONTRASENA"
         Me.TBCONTRASENA.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TBCONTRASENA.Size = New System.Drawing.Size(276, 27)
+        Me.TBCONTRASENA.Size = New System.Drawing.Size(276, 22)
         Me.TBCONTRASENA.TabIndex = 37
         '
         'TB1
         '
         Me.TB1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TB1.ForeColor = System.Drawing.Color.Gray
-        Me.TB1.Location = New System.Drawing.Point(231, 239)
-        Me.TB1.Multiline = True
+        Me.TB1.Location = New System.Drawing.Point(231, 227)
         Me.TB1.Name = "TB1"
         Me.TB1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TB1.Size = New System.Drawing.Size(276, 27)
+        Me.TB1.Size = New System.Drawing.Size(276, 22)
         Me.TB1.TabIndex = 38
         '
-        'Label1
+        'LUSUARIO
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(228, 114)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(50, 15)
-        Me.Label1.TabIndex = 39
-        Me.Label1.Text = "Usuario"
+        Me.LUSUARIO.AutoSize = True
+        Me.LUSUARIO.BackColor = System.Drawing.Color.White
+        Me.LUSUARIO.Enabled = False
+        Me.LUSUARIO.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LUSUARIO.ForeColor = System.Drawing.Color.DarkGray
+        Me.LUSUARIO.Location = New System.Drawing.Point(313, 104)
+        Me.LUSUARIO.Name = "LUSUARIO"
+        Me.LUSUARIO.Size = New System.Drawing.Size(50, 15)
+        Me.LUSUARIO.TabIndex = 39
+        Me.LUSUARIO.Text = "Usuario"
         '
-        'Label2
+        'LCONTRASENA
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(228, 163)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(107, 15)
-        Me.Label2.TabIndex = 40
-        Me.Label2.Text = "Correo electrónico"
+        Me.LCONTRASENA.AutoSize = True
+        Me.LCONTRASENA.BackColor = System.Drawing.Color.White
+        Me.LCONTRASENA.Enabled = False
+        Me.LCONTRASENA.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LCONTRASENA.ForeColor = System.Drawing.Color.DarkGray
+        Me.LCONTRASENA.Location = New System.Drawing.Point(434, 188)
+        Me.LCONTRASENA.Name = "LCONTRASENA"
+        Me.LCONTRASENA.Size = New System.Drawing.Size(70, 15)
+        Me.LCONTRASENA.TabIndex = 41
+        Me.LCONTRASENA.Text = "Contraseña"
         '
-        'Label3
+        'L1
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(228, 216)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(70, 15)
-        Me.Label3.TabIndex = 41
-        Me.Label3.Text = "Contraseña"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(228, 269)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(125, 15)
-        Me.Label4.TabIndex = 42
-        Me.Label4.Text = "Confirmar contraseña"
+        Me.L1.AutoSize = True
+        Me.L1.BackColor = System.Drawing.Color.White
+        Me.L1.Enabled = False
+        Me.L1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.L1.ForeColor = System.Drawing.Color.DarkGray
+        Me.L1.Location = New System.Drawing.Point(379, 231)
+        Me.L1.Name = "L1"
+        Me.L1.Size = New System.Drawing.Size(125, 15)
+        Me.L1.TabIndex = 42
+        Me.L1.Text = "Confirmar contraseña"
         '
         'BCREAR
         '
-        Me.BCREAR.Location = New System.Drawing.Point(251, 296)
+        Me.BCREAR.Location = New System.Drawing.Point(251, 333)
         Me.BCREAR.Name = "BCREAR"
         Me.BCREAR.Size = New System.Drawing.Size(95, 26)
         Me.BCREAR.TabIndex = 43
@@ -162,24 +157,37 @@ Partial Class Registro
         '
         'BCANCELAR
         '
-        Me.BCANCELAR.Location = New System.Drawing.Point(381, 296)
+        Me.BCANCELAR.Location = New System.Drawing.Point(381, 333)
         Me.BCANCELAR.Name = "BCANCELAR"
         Me.BCANCELAR.Size = New System.Drawing.Size(95, 26)
         Me.BCANCELAR.TabIndex = 44
         Me.BCANCELAR.Text = "Cancelar"
         Me.BCANCELAR.UseVisualStyleBackColor = True
         '
+        'LCORREO
+        '
+        Me.LCORREO.AutoSize = True
+        Me.LCORREO.BackColor = System.Drawing.Color.White
+        Me.LCORREO.Enabled = False
+        Me.LCORREO.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LCORREO.ForeColor = System.Drawing.Color.DarkGray
+        Me.LCORREO.Location = New System.Drawing.Point(397, 144)
+        Me.LCORREO.Name = "LCORREO"
+        Me.LCORREO.Size = New System.Drawing.Size(107, 15)
+        Me.LCORREO.TabIndex = 45
+        Me.LCORREO.Text = "Correo electrónico"
+        '
         'Registro
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(525, 343)
+        Me.ClientSize = New System.Drawing.Size(525, 383)
+        Me.Controls.Add(Me.LCORREO)
         Me.Controls.Add(Me.BCANCELAR)
         Me.Controls.Add(Me.BCREAR)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.L1)
+        Me.Controls.Add(Me.LCONTRASENA)
+        Me.Controls.Add(Me.LUSUARIO)
         Me.Controls.Add(Me.TB1)
         Me.Controls.Add(Me.TBCONTRASENA)
         Me.Controls.Add(Me.TBCORREO)
@@ -203,10 +211,10 @@ Partial Class Registro
     Friend WithEvents TBCORREO As TextBox
     Friend WithEvents TBCONTRASENA As TextBox
     Friend WithEvents TB1 As TextBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
+    Friend WithEvents LUSUARIO As Label
+    Friend WithEvents LCONTRASENA As Label
+    Friend WithEvents L1 As Label
     Friend WithEvents BCREAR As Button
     Friend WithEvents BCANCELAR As Button
+    Friend WithEvents LCORREO As Label
 End Class
